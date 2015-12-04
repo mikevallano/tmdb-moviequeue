26e958fd1264dcc6095666e03119f232d32502c5
mike vallano <mikevallano@gmail.com>
2015-12-03 18:27:31 -0600

    tweaked rspec config

--- spec/features/users_feature_spec.rb
+++ spec/features/users_feature_spec.rb
@@ -1,34 +1,17 @@
 require 'rails_helper'
-# include FeatureHelpers
 
-feature "Can sign up a new user and account" do
+feature "Can sign up a new user" do
   let(:email) { FFaker::Internet.email }
   let(:user) { FactoryGirl.create(:user) }
 
   scenario "user can successfully sign up" do
     sign_up_with(email, "password")
 
-    # visit root_path
-
-    # click_link 'Sign Up'
-
-    # fill_in "Email", with: email
-    # fill_in "Password", with: 'password'
-    # fill_in "Password confirmation", with: 'password'
-    # click_link_or_button 'Sign up'
-    # @email = email
     expect(page).to have_content("Signed in as: #{@email}")
   end
 
   scenario "existing users can sign in" do
-    # sign_in_user
-
-    visit root_path
-    click_link 'Sign In'
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: user.password
-    click_button 'Log in'
-    @current_user = user
+    sign_in_user
 
     expect(page).to have_content("Signed in as: #{@email}")
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,7 @@
 require 'devise'
-require 'support/controller_helpers'
-require 'support/feature_helpers'
+
 
 RSpec.configure do |config|
-  config.include ControllerHelpers, type: :controller
-  config.include FeatureHelpers, type: :feature
   Warden.test_mode!
 
   config.after do
