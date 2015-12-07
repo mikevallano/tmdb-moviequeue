1c29fd81a25078e0952439ad54e5eadcfd38d9f9
mike vallano <mikevallano@gmail.com>
2015-12-07 11:14:25 -0600

    tweaked tmdb controller spec

--- spec/controllers/tmdb_controller_spec.rb
+++ spec/controllers/tmdb_controller_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe TmdbController, type: :controller do
   describe "GET #more" do
     it "returns http success" do
       get :more
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(:redirect)
     end
   end
 
