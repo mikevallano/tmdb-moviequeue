5efde182e7a1121616683cdd81ceaf46d2971330
mike vallano <mikevallano@gmail.com>
2015-12-07 17:01:06 -0600

    refactored tmdb_feature_spec

--- spec/features/tmdb_feature_spec.rb
+++ spec/features/tmdb_feature_spec.rb
@@ -2,45 +2,36 @@ require 'rails_helper'
 
 feature "User can visit the search page" do
 
+ let(:email) { FFaker::Internet.email }
+
+
   scenario "users visits search page successfully" do
 
-    visit('tmdb/search')
+    sign_up_with(email, "password")
+    visit(api_search_path)
     expect(page).to have_content("Search the TMDB Database")
 
   end
 
   scenario "users searches for a movie and the API returns results" do
 
-    VCR.use_cassette('tmdb_search') do
+    sign_up_with(email, "password")
+    visit(api_search_path)
+    api_search_for_movie #method in features_helper
 
-      visit('tmdb/search')
-      fill_in "Enter Title", with: 'fargo'
-      click_button 'Search'
-      expect(page).to have_content("1996-04-05")
-      expect(page).to have_content("Fargo")
-
-    end
+    expect(page).to have_content("1996-04-05")
+    expect(page).to have_content("Fargo")
 
   end
 
   scenario "users clicks 'more info' and sees more info returned from API" do
 
-    VCR.use_cassette('tmdb_search') do
+    sign_up_with(email, "password")
+    visit(api_search_path)
+    api_search_for_movie #method in features_helper
 
-      visit('tmdb/search')
-      fill_in "Enter Title", with: 'fargo'
-      click_button 'Search'
-      expect(page).to have_content("1996-04-05")
-      expect(page).to have_content("Fargo")
-
-    end
-
-    VCR.use_cassette('tmdb_more') do
-
-      click_link('More info', match: :first)
-      expect(page).to have_content("Genres: Crime, Drama, Thriller")
-
-    end
+    api_more_info #method in features_helper
+    expect(page).to have_content("Genres: Crime, Drama, Thriller")
 
   end
 
