e31de095c24e13f034ae45d07ab6a673222d2f94
mike vallano <mikevallano@gmail.com>
2015-12-08 09:54:52 -0600

    passing listings create spec

--- spec/controllers/listings_controller_spec.rb
+++ spec/controllers/listings_controller_spec.rb
@@ -5,26 +5,28 @@ RSpec.describe ListingsController, type: :controller do
   let(:movie) { FactoryGirl.create(:movie) }
   let(:tmdb_id) { movie.tmdb_id }
   let(:valid_attributes) { {tmdb_id: tmdb_id} }
+  let(:listing) { FactoryGirl.create(:listing) }
 
 
   describe "GET #create" do
     it "creates a new listing" do
-      skip "trying to get this figured out"
       movie
       tmdb_id
-      poster = post :create, :listing => {tmdb_id: tmdb_id, movie_id: movie.id}
       expect {
-        post :create, :listing => {tmdb_id: tmdb_id}
+        post :create, :listing => { movie_id: movie.id }, tmdb_id: tmdb_id
       }.to change(Listing, :count).by(1)
     end
   end
 
   describe "GET #destroy" do
-    it "returns http success" do
-      skip "skip until the listings functionality is sorted out"
-      get :destroy
-      expect(response).to have_http_status(:success)
-    end
+    it "destroys the requested listing" do
+      listing
+      skip "need to figure this out"
+      expect {
+          delete :destroy, delete_listing_path("#{listing.list_id}", "#{listing.movie_id}")
+        }.to change(Listing, :count).by(-1)
+      end
   end
+
 
 end
