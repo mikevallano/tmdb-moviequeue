88e58accb3e90522f0e6e88dc2844de44c7a1c12
mike vallano <mikevallano@gmail.com>
2015-12-09 11:14:03 -0600

    added by_user_or_list method for tags

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -13,11 +13,19 @@ class Movie < ActiveRecord::Base
     joins(:lists).where(lists: { owner_id: user.id }).uniq
   end
 
-  def self.tagged_with(name, user)
-    Tag.find_by_name!(name).movies.by_user(user)
+  # def self.tagged_with(name, user)
+  #   Tag.find_by_name!(name).movies.by_user(user)
+  # end
+
+  # def tag_list(user)
+  #   tags.by_user(user).map(&:name)
+  # end
+
+  def self.tagged_with(name, userlist)
+    Tag.by_user_or_list(userlist).find_by_name!(name).movies.uniq
   end
 
-  def tag_list(user)
-    tags.by_user(user).map(&:name)
+  def tag_list(userlist)
+    tags.by_user_or_list(userlist).map(&:name)
   end
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -6,13 +6,23 @@ class Tag < ActiveRecord::Base
   has_many :movies, through: :taggings
   has_many :users, through: :taggings
 
-  def self.by_user(user)
-    joins(:taggings).where(taggings: { user_id: user.id }).uniq
-  end
+  # def self.by_user(user)
+  #   joins(:taggings).where(taggings: { user_id: user.id }).uniq
+  # end
 
   # def self.by_list(list)
   #   by_user(:user => List.find(list.id).members)
   # end
+
+  def self.by_user_or_list(userlist)
+    if userlist.is_a?(User) || !userlist.members.present?
+      joins(:taggings).where(taggings: { user_id: userlist.id }).uniq
+    else
+      userlist.members.each do |member|
+        joins(:taggings).where(taggings: { user_id: member.id }).uniq
+      end
+    end
+  end
 
   def self.first_or_create_tags(names)
     names.each do |n|
