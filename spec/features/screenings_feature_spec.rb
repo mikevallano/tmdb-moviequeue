7cb6bc9de1f3dc6f28ea122718495a58da72af20
mike vallano <mikevallano@gmail.com>
2015-12-14 20:04:39 -0600

    added screenings from the list show page

--- spec/features/screenings_feature_spec.rb
+++ spec/features/screenings_feature_spec.rb
@@ -9,8 +9,9 @@ feature "User can create a new screening" do
   let(:list2) { FactoryGirl.create(:list, owner_id: user2.id) }
   let(:listing) { FactoryGirl.create(:listing, list_id: list.id, movie_id: movie.id) }
   let(:listing2) { FactoryGirl.create(:listing, list_id: list2.id, movie_id: movie.id) }
+  let(:screening) { FactoryGirl.create(:screening, user_id: user.id, movie_id: movie.id, notes: "it was great") }
 
-  context "with signed in user" do
+  context "from the movie show page" do
 
     scenario "users can mark a movie as watched" do
 
@@ -51,7 +52,67 @@ feature "User can create a new screening" do
 
     end #view screening scenario
 
-  end #signed in user context
+  end #movie show page
+
+  context 'from the list show page' do
+
+    scenario 'users can mark a movie as watched' do
+
+      listing
+      list
+      movie
+
+      sign_in_user(user)
+      visit(list_path(list))
+      expect { click_link_or_button "Mark as watched" }.to change(Screening.by_user(user), :count).by(1)
+
+    end
+
+    scenario 'users can see movies that have been marked as watched' do
+
+      screening
+      listing
+      list
+      movie
+
+      sign_in_user(user)
+      visit(list_path(list))
+      expect(page).to have_content("Been watched.")
+      expect(page).not_to have_content("Mark as watched")
+
+    end
+
+    scenario 'users can create a new screening' do
+
+      listing
+      list
+      movie
+
+      sign_in_user(user)
+      visit(list_path(list))
+      click_link("Add a screening")
+      fill_in "Notes", with: "epic notes"
+      expect { click_link_or_button "Create Screening" }.to change(Screening.by_user(user), :count).by(1)
+      click_link "view my screenings"
+      expect(page).to have_content("epic notes")
+
+    end
+
+    scenario 'users can view existing screenings for a movie' do
+
+      screening
+      listing
+      list
+      movie
+
+      sign_in_user(user)
+      visit(list_path(list))
+      click_link("View my screenings")
+      expect(page).to have_content("it was great")
+
+    end
+
+  end
 
 
 end #feature
