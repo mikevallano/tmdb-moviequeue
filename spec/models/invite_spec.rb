2deaad0cd45d84f12426e7a151c40bd0b269cc87
mike vallano <mikevallano@gmail.com>
2015-12-15 16:54:19 -0600

    implemented invite model spec

--- spec/models/invite_spec.rb
+++ spec/models/invite_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Invite, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+  let(:invite) { FactoryGirl.build(:invite) }
+  let(:invalid_invite) { FactoryGirl.build(:invalid_invite) }
+  let(:invalid_email_invite) { FactoryGirl.build(:invalid_email_invite) }
+
+  context "with a valid factory" do
+
+    it "has a valid factory" do
+      expect(invite).to be_valid
+    end
+  end #valid factory context
+
+  context 'with an invalid factory' do
+
+    it { is_expected.to validate_presence_of(:email) }
+
+    it "has an invalid factory" do
+      expect(invalid_invite).not_to be_valid
+    end
+
+    it "validates format of email" do
+      expect(invalid_email_invite).not_to be_valid
+    end
+
+  end
+
+
+end #final
