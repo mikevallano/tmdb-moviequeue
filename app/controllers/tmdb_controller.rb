a52ef5175ed542981fe424fb1ff262fe8191f058
mike vallano <mikevallano@gmail.com>
2015-12-21 11:36:43 -0600

    added rescue to handle api call exceptions

--- app/controllers/tmdb_controller.rb
+++ app/controllers/tmdb_controller.rb
@@ -6,7 +6,7 @@ class TmdbController < ApplicationController
 
   def search
     if params[:movie_title]
-      @movie_title = params[:movie_title].gsub(' ', '-')
+      @movie_title = params[:movie_title]
       tmdb_handler_search(@movie_title)
     end
   end
@@ -22,15 +22,15 @@ class TmdbController < ApplicationController
 
   def actor_search
     if params[:name]
-      @name = params[:name].gsub(' ', '-')
+      @name = params[:name]
       tmdb_handler_actor_search(@name)
     end
   end
 
   def two_actor_search
     if params[:name_one] && params[:name_two]
-      @name_one = params[:name_one].gsub(' ', '-')
-      @name_two = params[:name_two].gsub(' ', '-')
+      @name_one = params[:name_one]
+      @name_two = params[:name_two]
       tmdb_handler_two_actor_search(@name_one, @name_two)
     end
   end
--- lib/tmdb_handler.rb
+++ lib/tmdb_handler.rb
@@ -4,6 +4,10 @@ module TmdbHandler
     @search_url = "http://api.themoviedb.org/3/search/movie?query=#{query}&api_key=#{ENV['tmdb_api_key']}"
     @tmdb_response = JSON.parse(open(@search_url).read, symbolize_names: true)
     @results = @tmdb_response[:results]
+    rescue
+      unless @tmdb_response.present?
+        @results = []
+      end
   end
 
   def tmdb_handler_movie_info(id)
@@ -25,7 +29,8 @@ module TmdbHandler
 
   def tmdb_handler_actor_search(name)
     @search_url = "https://api.themoviedb.org/3/search/person?query=#{name}&api_key=#{ENV['tmdb_api_key']}"
-    @actor_search_result = JSON.parse(open(@search_url).read, symbolize_names: true)[:results]
+     @tmdb_response = JSON.parse(open(@search_url).read, symbolize_names: true)
+     @actor_search_result = @tmdb_response[:results]
     if @actor_search_result.present?
       @id = @actor_search_result.first[:id]
       @search_id_url = "https://api.themoviedb.org/3/discover/movie?with_cast=#{@id}&sort_by=popularity.desc&api_key=#{ENV['tmdb_api_key']}"
@@ -33,19 +38,29 @@ module TmdbHandler
     else
       @been_in = []
     end
+    rescue
+      unless @tmdb_response.present?
+        @been_in = []
+      end
   end
 
   def tmdb_handler_two_actor_search(name_one, name_two)
     @search_url1 = "https://api.themoviedb.org/3/search/person?query=#{name_one}&api_key=#{ENV['tmdb_api_key']}"
     @search_url2 = "https://api.themoviedb.org/3/search/person?query=#{name_two}&api_key=#{ENV['tmdb_api_key']}"
-    @actor1_search_result = JSON.parse(open(@search_url1).read, symbolize_names: true)[:results]
-    @actor2_search_result = JSON.parse(open(@search_url2).read, symbolize_names: true)[:results]
+    @tmdb_response1 = JSON.parse(open(@search_url1).read, symbolize_names: true)
+    @tmdb_response2 = JSON.parse(open(@search_url2).read, symbolize_names: true)
+    @actor1_search_result = @tmdb_response1[:results]
+    @actor2_search_result = @tmdb_response2[:results]
     if @actor1_search_result.present? && @actor2_search_result.present?
       @id1 = @actor1_search_result.first[:id]
       @id2 = @actor2_search_result.first[:id]
       @search_ids_url = "https://api.themoviedb.org/3/discover/movie?with_people=#{@id1},#{@id2}&sort_by=revenue.desc&api_key=#{ENV['tmdb_api_key']}"
       @been_in = JSON.parse(open(@search_ids_url).read, symbolize_names: true)[:results]
     else
+      @been_in = []
+    end
+  rescue
+    unless @tmdb_response1.present? && @tmdb_response2.present?
       @been_in = []
     end
   end
