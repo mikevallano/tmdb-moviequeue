244f17d513165b71a6880d6a77f61ff71b5e8bb4
mike vallano <mikevallano@gmail.com>
2015-12-31 17:20:22 -0600

    changed per_page to 20

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,18 +2,19 @@ class MoviesController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @per_page = 20
     if params["tag"]
       if params[:list_id]
         @list = List.find(params[:list_id])
-        @movies = @list.movies.tagged_with(params['tag'], @list).paginate(:page => params[:page], :per_page => 1)
+        @movies = @list.movies.tagged_with(params['tag'], @list).paginate(:page => params[:page], :per_page => @per_page)
       else
-        @movies = current_user.movies.tagged_with(params["tag"], current_user).paginate(:page => params[:page], :per_page => 1)
+        @movies = current_user.movies.tagged_with(params["tag"], current_user).paginate(:page => params[:page], :per_page => @per_page)
       end
     else
-      @movies = current_user.all_movies.paginate(:page => params[:page], :per_page => 1)
+      @movies = current_user.all_movies.paginate(:page => params[:page], :per_page => @per_page)
     end
     if params[:genre]
-      @movies = current_user.movies.by_genre(params[:genre]).paginate(:page => params[:page], :per_page => 1)
+      @movies = current_user.movies.by_genre(params[:genre]).paginate(:page => params[:page], :per_page => @per_page)
     end
   end
 
