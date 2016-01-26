34620edf5fae341a5616f5b3518733b504db9210
anne <richardson.ae@gmail.com>
2016-01-25 22:00:41 -0600

    page title content_for

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
  def page_title
    if content_for?(:title)
-     "ReactBetter - #{content_for(:title)}"
+     "MovieQueue - #{content_for(:title)}"
     else
-     "ReactBetter - Choose a Mindful, Happier Life"
+     "MovieQueue - Personal Movie Playlist"
    end #content_for
  end #page_title
 end #ApplicationHelper
