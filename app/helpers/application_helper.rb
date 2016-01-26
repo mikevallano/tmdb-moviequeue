4098bd3a800994de1e9952a70ccb12aba13f6c47
anne <richardson.ae@gmail.com>
2016-01-25 21:17:30 -0600

    bootstrap, fontawesome, googlefonts, page_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
-end
+ def page_title
+   if content_for?(:title)
+     "ReactBetter - #{content_for(:title)}"
+    else
+     "ReactBetter - Choose a Mindful, Happier Life"
+   end #content_for
+ end #page_title
+end #ApplicationHelper
