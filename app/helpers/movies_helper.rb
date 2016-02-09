3122ea8d3f2a839caee2d5d247132745d91eb008
anne <richardson.ae@gmail.com>
2016-02-09 17:43:13 -0600

    updated movie poster image size

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -2,7 +2,7 @@ module MoviesHelper
 
   def image_for(movie)
     if movie.poster_path.present?
-      image_tag("http://image.tmdb.org/t/p/w154#{movie.poster_path}")
+      image_tag("http://image.tmdb.org/t/p/w185#{movie.poster_path}")
     else
       image_tag('placeholder.png')
     end #if
