acb698aa4ba0a0dd6175f6a4a26f7c28d67c52f0
mike vallano <mikevallano@gmail.com>
2016-02-10 11:22:11 -0600

    updated tmdb feature specs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -16,26 +16,11 @@ require 'support/feature_helpers'
 require 'support/mailer_helpers'
 require 'support/vcr'
 require 'capybara/email/rspec'
-# Add additional requires below this line. Rails is not loaded until this point!
+require 'support/wait_for_ajax'
 
-# Requires supporting ruby files with custom matchers and macros, etc, in
-# spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
-# run as spec files by default. This means that files in spec/support that end
-# in _spec.rb will both be required and run as specs, causing the specs to be
-# run twice. It is recommended that you do not name files matching this glob to
-# end with _spec.rb. You can configure this pattern with the --pattern
-# option on the command line or in ~/.rspec, .rspec or `.rspec-local`.
-#
-# The following line is provided for convenience purposes. It has the downside
-# of increasing the boot-up time by auto-requiring all files in the support
-# directory. Alternatively, in the individual `*_spec.rb` files, manually
-# require only the support files necessary.
-#
-# Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
-
-# Checks for pending migrations before tests are run.
-# If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
+
+Capybara.default_max_wait_time = 4
 
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
@@ -50,6 +35,7 @@ RSpec.configure do |config|
   config.include ControllerHelpers, type: :controller
   config.include FeatureHelpers, type: :feature
   config.include MailerHelpers
+  config.include WaitForAjax, type: :feature
   config.before(:each) { reset_mailer }
 
 
