f7712498762383807ce71ad2f3367806fb5086d7
mike vallano <mikevallano@gmail.com>
2016-03-31 09:31:41 -0500

    configed default timezone

--- config/application.rb
+++ config/application.rb
@@ -41,6 +41,7 @@ module TmdbMoviequeue
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.autoload_paths += %W(#{config.root}/lib)
+    config.time_zone = 'Central Time (US & Canada)'
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
