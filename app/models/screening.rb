0be08f545430fe4a21d7eacc5b6fd6644e1fdeca
mike vallano <mikevallano@gmail.com>
2016-03-31 09:47:58 -0500

    commented default screening date

--- app/models/screening.rb
+++ app/models/screening.rb
@@ -11,6 +11,7 @@ class Screening < ActiveRecord::Base
   before_save :default_date
 
   def default_date
+    # time is set to US Central in application.rb
     self.date_watched ||= DateTime.now.to_date
   end
 end
