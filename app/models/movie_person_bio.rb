23a8f09c3971b2ab80259058bfacbb0b4162e6aa
mike vallano <mikevallano@gmail.com>
2016-06-28 20:15:54 -0500

    Refactor Actor Bio with Line Breaks
    Refactor biirthday formatting to model rather than view.

--- app/models/movie_person_bio.rb
+++ app/models/movie_person_bio.rb
@@ -15,11 +15,18 @@ class MoviePersonBio
   def self.parse_result(result)
     @id = result[:id]
     @name = result[:name]
-    @bio = result[:biography]
-    @birthday = Date.parse(result[:birthday]) if result[:birthday].present?
+    @bio = result[:biography].present? ? MoviePersonBio.parse_bio(result[:biography]) : "No birthday available."
+    @birthday = result[:birthday].present? ? Date.parse(result[:birthday]).stamp("June 9th, 2001") : "No bio available."
     @profile_path = result[:profile_path]
 
     MoviePersonBio.new(@id, @name, @bio, @birthday, @profile_path)
   end #parse result
+
+
+  def self.parse_bio(bio)
+    bio.gsub!(/(From Wikipedia, the free encyclopedia.?)\s+/, '')
+    bio.gsub!(/(Description above from).*Wikipedia\s.*\./, "Bio from Wikipedia.")
+    bio.gsub(/(?:\n\r?|\r\n?)/, '<br>').html_safe
+  end
 
 end #class
