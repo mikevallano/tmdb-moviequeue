f0ae02d24f514935cf20f1900c1503059e21442f
anne <richardson.ae@gmail.com>
2018-09-03 12:37:54 -0500

    Tests for validations

--- spec/models/movie_person_profile_spec.rb
+++ spec/models/movie_person_profile_spec.rb
@@ -3,14 +3,50 @@
 require 'rails_helper'
 
 RSpec.describe MoviePersonProfile, type: :model do
-  describe 'a valid movie person profile' do
-    let(:movie_person_profile) { build(:movie_person_profile) }
+  let(:movie_person_profile) { build(:movie_person_profile) }
 
+  describe 'a valid movie person profile' do
     context 'when has valid params' do
       it 'is valid' do
         expect(movie_person_profile).to be_valid
       end
     end
+
+    context 'when does not have a person_id' do
+      it 'is invalid' do
+        movie_person_profile.person_id = nil
+        expect(movie_person_profile).to_not be_valid
+      end
+    end
+
+    context 'when does not have a name' do
+      it 'is invalid' do
+        movie_person_profile.name = nil
+        expect(movie_person_profile).to_not be_valid
+      end
+    end
+
+    context 'when does not have a bio' do
+      it 'is invalid' do
+        movie_person_profile.bio = nil
+        expect(movie_person_profile).to_not be_valid
+      end
+    end
+
+    context 'when does not have a birthday_and_age' do
+      it 'is invalid' do
+        movie_person_profile.birthday_and_age = nil
+        expect(movie_person_profile).to_not be_valid
+      end
+    end
+
+    context 'when does not have a profile_path' do
+      it 'is invalid' do
+        movie_person_profile.profile_path = nil
+        expect(movie_person_profile).to_not be_valid
+      end
+    end
+  end
   end
     # let(:reading_card) { ReadingCard.new(reading_card_params) }
     # let(:card) { build(:card) }
