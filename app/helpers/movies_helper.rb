46c7af1ba7c82f8a46ed79b6da6e2c0d36c70adc
richardsonae <richardson.ae@gmail.com>
2020-05-10 10:43:35 -0500

    Add background color an dmovie name to missing image (#194)

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -2,7 +2,7 @@ module MoviesHelper
 
   def image_for(movie)
     if movie.poster_path.present?
-      image_tag("http://image.tmdb.org/t/p/w185#{movie.poster_path}")
+      image_tag("http://image.tmdb.org/t/p/w185#{movie.poster_path}", title: movie.title, alt: movie.title)
     else
       render "movies/movie_missing_poster", movie: movie
       # image_tag('placeholder.png')
