a7066cc6404a418497b7811357ce2002d03d8354
mike vallano <mikevallano@gmail.com>
2021-11-11 08:17:04 -0600

    Dont record new episodes by default

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -3,5 +3,4 @@ VCR.configure do |config|
   config.hook_into :webmock
   config.allow_http_connections_when_no_cassette = true
   config.ignore_localhost = true
-  config.default_cassette_options = { record: :new_episodes }
 end
