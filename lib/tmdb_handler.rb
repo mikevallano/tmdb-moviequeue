27fad116daad18fcd78ec1bf8f365fc549938bfd
Anne Richardson <richardson.ae@gmail.com>
2021-12-23 09:34:50 -0600

    WIP add tmdb_handler

--- lib/tmdb_handler.rb
+++ lib/tmdb_handler.rb
@@ -181,6 +181,18 @@ module TmdbHandler
     )
   end
 
+  def tmdb_handler_tv_episode(series_id:, season_number:, episode_number:)
+    # https://api.themoviedb.org/3/tv/{tv_id}/season/{season_number}/episode/{episode_number}?api_key=3fb5f9a5dbd80d943fdccf6bd1e7f188&language=en-US
+
+    episode_url = "#{BASE_URL}/tv/#{series_id}/season/#{season_number}/episode/#{episode_number}?api_key=#{ENV['tmdb_api_key']}"
+    episode_data = JSON.parse(open(episode_url).read, symbolize_names: true)
+    TVSeason.parse_record(
+      series: series,
+      show_id: show_id,
+      episode_data: episode_data
+    )
+  end
+
   def tmdb_handler_two_movie_search(movie_one, movie_two)
     tmdb_handler_search(movie_one)
       if @movies.present?
