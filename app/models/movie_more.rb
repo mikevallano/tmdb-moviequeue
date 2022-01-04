625a2f816687a28fa49e25bc5eecbda31fc18e0a
Anne Richardson <richardson.ae@gmail.com>
2022-01-04 16:28:59 -0600

    Reduce instance variables in MovieMore class

--- app/models/movie_more.rb
+++ app/models/movie_more.rb
@@ -1,104 +1,75 @@
+# frozen_string_literal: true
+
 class MovieMore
-  def initialize(title, in_db, tmdb_id, release_date, vote_average, genres, overview, actors,
-    backdrop_path, poster_path, trailer, imdb_id, mpaa_rating, director,
-    director_id, adult, popularity, runtime, tags, lists)
+  attr_accessor :actors, :adult, :backdrop_path, :crew, :director, :director_id, :genres, :imdb_id, :lists, :mpaa_rating, :overview, :popularity, :poster_path, :production_companies, :release_date, :runtime, :tags, :title, :tmdb_id, :trailer, :vote_average
 
-    @title = title
-    @in_db = in_db
-    @tmdb_id = tmdb_id
-    @release_date = release_date
-    @vote_average = vote_average
-    @genres = genres
-    @overview = overview
-    @actors = actors
-    @backdrop_path = backdrop_path
-    @poster_path = poster_path
-    @trailer = trailer
-    @imdb_id = imdb_id
-    @mpaa_rating = mpaa_rating
-    @director = director
-    @director_id = director_id
-    @adult = adult
-    @popularity = popularity
-    @runtime = runtime
-    @tags = tags
-    @lists = lists
-
+  def initialize(args)
+    @actors = args[:actors]
+    @adult = args[:adult]
+    @backdrop_path = args[:backdrop_path]
+    @crew = args[:crew]
+    @director = args[:director]
+    @director_id = args[:director_id]
+    @lists = args[:lists]
+    @imdb_id = args[:imdb_id]
+    @genres = args[:genres]
+    @overview = args[:overview]
+    @release_date = args[:release_date]
+    @mpaa_rating = args[:mpaa_rating]
+    @popularity = args[:popularity]
+    @poster_path = args[:poster_path]
+    @production_companies = args[:production_companies]
+    @runtime = args[:runtime]
+    @tags = args[:tags]
+    @title = args[:title]
+    @tmdb_id = args[:tmdb_id]
+    @trailer = args[:trailer]
+    @vote_average = args[:vote_average]
   end
-
-  attr_accessor :title, :in_db, :tmdb_id, :release_date, :vote_average, :genres, :overview,
-  :actors, :backdrop_path, :poster_path, :trailer, :imdb_id, :mpaa_rating, :director,
-  :director_id, :adult, :popularity, :runtime, :tags, :lists
 
   def self.parse_result(result)
-    @tmdb_id = result[:id]
-    @in_db = Movie.exists?(tmdb_id: @tmdb_id)
-      if @in_db
-        @movie = Movie.find_by(tmdb_id: @tmdb_id)
-      else
-        self.tmdb_info(result)
-      end #if in_db
-    @movie
+    Movie.find_by(tmdb_id: result[:id]) || tmdb_info(result)
   end
 
-    def times_seen_by(user)
-      0
+  def times_seen_by(user)
+    0
+  end
+
+  def self.tmdb_info(result)
+    release_date = Date.parse(result[:release_date]) if result[:release_date].present?
+    vote_average = result[:vote_average].present? ? result[:vote_average].round(1) : 0
+    runtime = result[:runtime].presence || 0
+
+    trailer = if result[:trailers][:youtube].present?
+      result[:trailers][:youtube][0][:source].presence
     end
 
-    def self.tmdb_info(result)
-        @title = result[:title]
-        @release_date = Date.parse(result[:release_date]) if result[:release_date].present?
-        if result[:vote_average].present?
-          @vote_average = result[:vote_average].round(1)
-        else
-          @vote_average = 0
-        end
-        @genres = result[:genres].map { |genre| genre[:name] }
-        @overview = result[:overview]
-        @actors = result[:credits][:cast].map { |cast| cast[:name] }
-        @backdrop_path = result[:backdrop_path]
-        @poster_path = result[:poster_path]
-        if result[:trailers][:youtube].present?
-          if result[:trailers][:youtube][0][:source].present?
-            @trailer = result[:trailers][:youtube][0][:source]
-          else
-            @trailer = nil
-          end
-        else
-          @trailer = nil
-        end
-        @imdb_id = result[:imdb_id]
-        @adult = result[:adult]
-        @popularity = result[:popularity]
-        if result[:runtime].present?
-          @runtime = result[:runtime]
-        else
-          @runtime = 0
-        end
-        @tags = nil
-        @lists = nil
+    us_movie_releases = result[:releases][:countries].select { |country| country[:iso_3166_1] == "US" }
+    mpaa_rating = us_movie_releases.present? && us_movie_releases.first[:certification].present? ? us_movie_releases.first[:certification] : 'NR'
+    first_director = result[:credits][:crew].find {|crew| crew[:department] == 'Directing'}
 
-        if result[:releases][:countries].select { |country| country[:iso_3166_1] == "US" }.present?
-          if result[:releases][:countries].select { |country| country[:iso_3166_1] == "US" }.first[:certification].present?
-            @mpaa_rating = result[:releases][:countries].select { |country| country[:iso_3166_1] == "US" }.first[:certification]
-          else
-            @mpaa_rating = "NR"
-          end
-        else
-          @mpaa_rating = "NR"
-        end
-
-        @crew = result[:credits][:crew]
-        @crew.find do |crew|
-          if crew[:department] == "Directing"
-            @director = crew[:name]
-            @director_id = crew[:id]
-          end
-        end
-
-        @movie = MovieMore.new(@title, @in_db, @tmdb_id, @release_date, @vote_average, @genres,
-          @overview, @actors, @backdrop_path, @poster_path, @trailer, @imdb_id, @mpaa_rating,
-          @director, @director_id, @adult, @popularity, @runtime, @tags, @lists)
-    end
-
+    new(
+      actors: result[:credits][:cast].map { |cast| cast[:name] },
+      adult: result[:adult],
+      backdrop_path: result[:backdrop_path],
+      crew: result[:credits][:crew],
+      director: first_director[:name],
+      director_id: first_director[:id],
+      genres: result[:genres].map { |genre| genre[:name] },
+      imdb_id: result[:imdb_id],
+      lists: nil,
+      mpaa_rating: mpaa_rating,
+      overview: result[:overview],
+      popularity: result[:popularity],
+      poster_path: result[:poster_path],
+      production_companies: result[:production_companies],
+      release_date: release_date,
+      runtime: runtime,
+      tags: nil,
+      title: result[:title],
+      tmdb_id: result[:id],
+      trailer: trailer,
+      vote_average: vote_average
+    )
+  end
 end
