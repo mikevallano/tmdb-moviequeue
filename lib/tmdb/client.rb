edaf157cffb94de36cc596c0d19b625ff6f053a4
Anne Richardson <richardson.ae@gmail.com>
2022-02-12 16:55:09 -0600

    Incorporate changes from PR #292 to help with merging

--- lib/tmdb/client.rb
+++ lib/tmdb/client.rb
@@ -53,11 +53,9 @@ module Tmdb
         end
       end
 
-      def movie(movie_id)
-        data = get_parsed_movie_data(movie_id)
       def get_movie_data(tmdb_movie_id)
         data = get_parsed_movie_data(tmdb_movie_id)
-        MovieMore.parse_result(data)
+        MovieMore.initialize_from_parsed_data(data)
       end
 
       def movie_cast(tmdb_movie_id)
@@ -90,7 +88,7 @@ module Tmdb
           raise Error.new("API request failed for movie: #{movie.title}. tmdb_id: #{tmdb_id}")
         end
 
-        updated_data = MovieMore.tmdb_info(api_result)
+        updated_data = MovieMore.initialize_from_parsed_data((api_result)
 
         if movie.title != updated_data.title
           puts "Movie title doesn't match. tmdb_id: #{tmdb_id}. Current title: #{movie.title}. Title in TMDB: #{updated_data.title}"
@@ -126,8 +124,8 @@ module Tmdb
         if not_found_message.present?
           OpenStruct.new(not_found_message: not_found_message)
         else
-          movie_one = get_movie_data(movie_one_results.movies.first.tmdb_id)
-          movie_two = get_movie_data(movie_two_results.movies.first.tmdb_id)
+          movie_one = GuaranteedMovie.find_or_initialize_from_api(movie_one_results.movies.first.tmdb_id)
+          movie_two = GuaranteedMovie.find_or_initialize_from_api(movie_two_results.movies.first.tmdb_id)
           OpenStruct.new(
             movie_one: movie_one,
             movie_two: movie_two,
