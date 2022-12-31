05c9872c3e43759e055b03abf4883e39b798780a
Anne Richardson <richardson.ae@gmail.com>
2022-12-31 11:36:57 -0600

    Move tags and lists to the initialize block

--- app/models/movie_search.rb
+++ app/models/movie_search.rb
@@ -1,7 +1,7 @@
 class MovieSearch
   attr_accessor :title, :in_db, :tmdb_id, :release_date, :vote_average, :overview, :backdrop_path, :poster_path, :tags, :lists
 
-  def initialize(title:, in_db:, tmdb_id:, release_date:, vote_average:, overview:, backdrop_path:, poster_path:, tags:, lists:)
+  def initialize(title:, in_db:, tmdb_id:, release_date:, vote_average:, overview:, backdrop_path:, poster_path:)
     @title = title
     @in_db = in_db
     @tmdb_id = tmdb_id
@@ -10,8 +10,8 @@ class MovieSearch
     @overview = overview
     @backdrop_path = backdrop_path
     @poster_path = poster_path
-    @tags = tags
-    @lists = lists
+    @tags = nil
+    @lists = nil
   end
 
   def self.parse_results(results)
@@ -30,9 +30,7 @@ class MovieSearch
           vote_average: result[:vote_average]&.round(1),
           overview: result[:overview],
           backdrop_path: result[:backdrop_path],
-          poster_path: result[:poster_path],
-          tags: nil,
-          lists: nil
+          poster_path: result[:poster_path]
         )
       end
     end
