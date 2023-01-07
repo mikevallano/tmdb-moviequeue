d9dbf4638738103ab4bae9595346c371783e5aa1
Anne Richardson <richardson.ae@gmail.com>
2023-01-07 15:07:00 -0600

    Configure new Sentry gems

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,8 +1,5 @@
-if Rails.env == 'production'
-  Raven.configure do |config|
-    config.dsn = ENV["SENTRY_DSN"]
-    config.environments = ['production']
-    config.should_capture = Proc.new { |e| Rails.env == 'production' }# unless e.contains_sensitive_info? }
-    config.tags = { environment: Rails.env }
-  end
+Sentry.init do |config|
+  config.dsn = ENV['SENTRY_DSN']
+  config.enabled_environments = %w[production]
+  config.breadcrumbs_logger = [:active_support_logger]
 end
