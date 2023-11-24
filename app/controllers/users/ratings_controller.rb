8c91103040f0ccf2f105e725064b2d4f47d927e0
Anne Richardson <richardson.ae@gmail.com>
2023-11-24 15:33:32 -0500

    Add pagination

--- app/controllers/users/ratings_controller.rb
+++ app/controllers/users/ratings_controller.rb
@@ -2,6 +2,9 @@ class Users::RatingsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @ratings = current_user.ratings.where(value: 8..Float::INFINITY).order(value: :desc)
+    @ratings = current_user.ratings
+      .where(value: 8..Float::INFINITY)
+      .order(value: :desc)
+      .paginate(:page => params[:page], per_page: 20)
   end
 end
