99fd314a240c1e7ad2c0682dc4f2a03745a20e63
richardsonae <richardson.ae@gmail.com>
2023-11-24 15:09:18 -0600

    Add my highest rated movies page (#377)
    Create new user movie ratings page

---
+++ app/controllers/users/ratings_controller.rb
@@ -0,0 +1,11 @@
+class Users::RatingsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @ratings = current_user.ratings
+      .includes(:movie)
+      .where(value: 8..Float::INFINITY)
+      .order(value: :desc)
+      .paginate(page: params[:page], per_page: 20)
+  end
+end
