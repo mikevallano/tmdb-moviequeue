4c2649eddd832d66b2661dc18c8059c0789e7744
mike vallano <mikevallano@gmail.com>
2023-12-11 13:47:07 -0500

    391: Conditionally set waitTime based on Rails env

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -8,12 +8,22 @@ export default class extends Controller {
     // Since we do a request to the controller to load the
     // modal into the DOM, the delay is to wait until the
     // modal is in the DOM before tring to open it.
+
+    // In production, the response is much faster, which is
+    // the purpose of the waitTime difference.
+
     // This logic can go away when we refactor how we're
     // handling modals, and/or writing our own.
+
+    const waitTime =
+      document.head.querySelector('meta[name=rails_env]').content ===
+      'production'
+        ? 400
+        : 900
     const modalId = this.element.dataset.target
     setTimeout(() => {
       $(`${modalId}`).modal('show')
-    }, 400)
+    }, waitTime)
   }
 
   close() {
