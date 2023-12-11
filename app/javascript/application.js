e22d2fb8913346cf477ba3bf92d8d4ed3d44e4f9
mike vallano <mikevallano@gmail.com>
2023-12-11 13:38:08 -0500

    391: Get rid of console logs

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,16 +4,3 @@ import 'jquery'
 import 'jquery-ui'
 import 'bootstrap'
 import './autocomplete.js'
-
-document.addEventListener('turbo:load', () => {
-  console.log('loaded turbo!')
-})
-
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('DOM loaded')
-  $('.dropdown-toggle').dropdown()
-})
-
-$(document).on('turbo:load', () => {
-  console.log('turbo via jquery!')
-})
