4b50d55c6acd3c0688c39fbe1ac812c6c40b82e5
Anne Richardson <richardson.ae@gmail.com>
2024-12-15 10:05:07 -0500

    Edit TV series viewings

--- app/controllers/tv_series_viewings_controller.rb
+++ app/controllers/tv_series_viewings_controller.rb
@@ -1,12 +1,16 @@
 class TVSeriesViewingsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_tv_series_viewing, only: [:edit, :update, :destroy]
 
   def index
-    @viewings = current_user.tv_series_viewings
+    @tv_series_viewings = current_user.tv_series_viewings
+  end
+
+  def edit
   end
 
   def create
-    @viewing = current_user.tv_series_viewings.new(
+    @tv_series_viewing = current_user.tv_series_viewings.new(
       title: params[:title],
       show_id: params[:show_id],
       url: tv_series_path(show_id: params[:show_id]),
@@ -14,7 +18,7 @@ class TVSeriesViewingsController < ApplicationController
     )
 
     respond_to do |format|
-      if @viewing.save
+      if @tv_series_viewing.save
         format.turbo_stream {}
         format.html { redirect_to tv_series_path(show_id: params[:show_id]), notice: 'Added to Currently Watching List.' }
       else
@@ -24,15 +28,29 @@ class TVSeriesViewingsController < ApplicationController
   end
 
   def update
-    @viewing = current_user.tv_series_viewings.active.find_by(show_id: params[:show_id])
-
     respond_to do |format|
-      if @viewing.update(ended_at: Time.current)
+      if @tv_series_viewing.update(tv_series_viewing_params)
+        @active_tv_series_viewing = current_user.tv_series_viewings.active.find_by(show_id: @tv_series_viewing.show_id)
         format.turbo_stream {}
-        format.html { redirect_to tv_series_path(show_id: params[:show_id]), notice: 'Removed from Currently Watching List.' }
+        format.html { redirect_to tv_series_viewings_path, notice: 'Removed from Currently Watching List.' }
       else
-        format.html { redirect_to tv_series_path(show_id: params[:show_id]), error: 'Could not remove from Currently Watching List.' }
+        format.html { redirect_to tv_series_viewings_path, error: 'Could not remove from Currently Watching List.' }
       end
     end
+  end
+
+  def destroy
+    @tv_series_viewing.destroy
+    redirect_to tv_series_viewings_path, notice: 'Successfully deleted viewing.'
+  end
+
+  private
+
+  def set_tv_series_viewing
+    @tv_series_viewing = current_user.tv_series_viewings.find(params[:id])
+  end
+
+  def tv_series_viewing_params
+    params.require(:tv_series_viewing).permit(:show_id, :title, :started_at, :ended_at, :user_id)
   end
 end
