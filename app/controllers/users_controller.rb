0061a947365d116b731d92122b9b20c50d763775
Anne Richardson <richardson.ae@gmail.com>
2025-01-05 14:34:57 -0500

    Add edit page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_user, only: [:show, :edit]
-  before_action :restrict_to_content_owner!, only: [:show, :edit]
+  before_action :set_user, only: [:show, :edit, :update]
+  before_action :restrict_to_content_owner!, only: [:show, :edit, :update]
 
   def show
     if request.path != user_path(@user)
@@ -12,9 +12,29 @@ class UsersController < ApplicationController
   def edit
   end
 
+  def update
+    if @user.update(user_params)
+      selected_streaming_service_provider_ids = params[:user][:streaming_service_provider_ids]
+
+      if selected_streaming_service_provider_ids.any?
+        @user.user_streaming_service_providers.delete_all
+        selected_streaming_service_provider_ids.reject(&:blank?).each do |id|
+          @user.user_streaming_service_providers.create!(streaming_service_provider_id: id)
+        end
+      end
+      redirect_to user_url(@user), notice: "Settings Updated"
+    else
+      render :edit, alert: 'Failed to update preferences.'
+    end
+  end
+
   private
 
   def set_user
     @user = User.friendly.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :slug, :default_location)
   end
 end
