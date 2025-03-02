a3214c24e3b857136e06ab8e6d451bdea6cd8bbc
mike vallano <mikevallano@gmail.com>
2025-03-02 10:44:23 -0500

    471: Revert "Add indexes to listings table"

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_01_135740) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_02_154214) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,7 +43,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_01_135740) do
     t.datetime "updated_at", precision: nil, null: false
     t.integer "user_id"
     t.integer "priority"
-    t.index ["movie_id", "list_id"], name: "index_listings_on_movie_id_and_list_id", unique: true
+    t.index ["list_id"], name: "index_listings_on_list_id"
+    t.index ["movie_id"], name: "index_listings_on_movie_id"
   end
 
   create_table "lists", force: :cascade do |t|
