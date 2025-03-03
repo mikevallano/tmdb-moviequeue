fe387f1ba5e6f61a10c2d12e03d434be4fdb2181
Anne Richardson <richardson.ae@gmail.com>
2025-03-03 11:11:59 -0500

    Use API call to indicate main cast status

--- app/entities/tv_actor_credit.rb
+++ app/entities/tv_actor_credit.rb
@@ -16,7 +16,7 @@ class TVActorCredit
   end
 
   def self.parse_record(record)
-    seasons = TVCreditSeason.parse_records(record[:media][:seasons]).presence if record[:media][:seasons].present?
+    seasons = TVCreditSeason.parse_records(record[:media][:seasons]).sort_by {|s| s.name}
     episodes = TVEpisode.parse_records(record[:media][:episodes]).presence if record[:media][:episodes].present?
     episodes_by_number = episodes.present? ? episodes.sort_by { |e|e.episode_number } : []
     new(
@@ -33,6 +33,15 @@ class TVActorCredit
   end
 
   def in_main_cast?
-    @character.present? && @episodes.empty?
+    # If an actor's character doesn't have a name, then they are not in the main cast.
+    return false if @character.empty?
+    # When an actor's episodes array is empty and the character has a name, they are in the main cast.
+    return true if @episodes.empty?
+    
+    # There are cases when an actor appeared as a guest before or after being in the main cast. 
+    # We reserve making this API call only for that condition. Note the actor must be in the final
+    # series cast data, so if they were replaced by another actor, this will return false.
+    series_data = TVSeriesDataService.get_tv_series_data(@show_id)
+    series_data.actors.map(&:actor_id).include?(@actor_id)
   end
 end
--- spec/entities/tv_actor_credit_spec.rb
+++ spec/entities/tv_actor_credit_spec.rb
@@ -3,36 +3,47 @@
 require 'rails_helper'
 
 RSpec.describe TVActorCredit, type: :model do
-  describe "in_main_cast?" do
-    context "when there is no character and there are episodes" do
-      let(:tv_actor_credit) { build(:tv_actor_credit, :with_episodes, character: "") }
-      
-      it "returns false" do
-        expect(tv_actor_credit.in_main_cast?).to eq(false)
-      end
-    end
+  let(:actor_id) { 1234 }
 
-    context "when there is no character and there are no episodes" do
-      let(:tv_actor_credit) { build(:tv_actor_credit, episodes: [], character: "") }
+  describe "in_main_cast?" do
+    context "when there is a character but there are no episodes" do
+      let(:tv_actor_credit) { build(:tv_actor_credit, episodes: [], actor_id: actor_id, character: "foo") }
       
-      it "returns false" do
-        expect(tv_actor_credit.in_main_cast?).to eq(false)
+      it "returns true" do
+        expect(tv_actor_credit.in_main_cast?).to eq(true)
       end
     end
 
     context "when there is a character and there are episodes" do
-      let(:tv_actor_credit) { build(:tv_actor_credit, :with_episodes, character: "foo") }
+      let(:tv_actor_credit) { build(:tv_actor_credit, :with_episodes, actor_id: actor_id, character: "foo") }
       
-      it "returns false" do
-        expect(tv_actor_credit.in_main_cast?).to eq(false)
+      context "and the person appears in the series cast credits" do
+        before { allow(TVSeriesDataService).to receive(:get_tv_series_data).and_return(series_data) }
+        
+        let(:series_data) { OpenStruct.new( actors: [OpenStruct.new(actor_id: actor_id)]) }
+       
+        it "returns true" do
+          expect(tv_actor_credit.in_main_cast?).to eq(true)
+        end
+      end
+
+      context "and the person does not appear in the series cast credits" do
+        before { allow(TVSeriesDataService).to receive(:get_tv_series_data).and_return(series_data) }
+        
+        let(:series_data) { OpenStruct.new( actors: [OpenStruct.new(actor_id: other_actor_id)]) }
+        let(:other_actor_id) { 5678 }
+       
+        it "returns false" do
+          expect(tv_actor_credit.in_main_cast?).to eq(false)
+        end
       end
     end
 
-    context "when there is a character but there are no episodes" do
-      let(:tv_actor_credit) { build(:tv_actor_credit, episodes: [], character: "foo") }
+    context "when there is no character" do
+      let(:tv_actor_credit) { build(:tv_actor_credit, :with_episodes, actor_id: actor_id, character: "") }
       
-      it "returns true" do
-        expect(tv_actor_credit.in_main_cast?).to eq(true)
+      it "returns false" do
+        expect(tv_actor_credit.in_main_cast?).to eq(false)
       end
     end
   end
