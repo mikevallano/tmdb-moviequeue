cbc6bea9bcc241365f00eff4f64ec5adb77c4308
Anne Richardson <richardson.ae@gmail.com>
2026-06-19 10:46:56 -0400

    Disable assume_ssl to fix Turbo POST 422 errors behind Heroku SSL proxy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,7 +25,8 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
-  config.assume_ssl = true
+  # Disabled: causes CSRF 422 errors with Turbo POST requests behind Heroku SSL proxy
+  config.assume_ssl = false
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
