c8a91b09aa4d731908b463c2050f143f8b5507bd
richardsonae <richardson.ae@gmail.com>
2026-07-29 14:19:57 -0400

    Revert "Upgrade From Rails 7.2.3.1 to 8.0.0 (#509)" (#513)
    This reverts commit eba12c75524a262032b11a4c7a51cdbf02d4fa3e.

--- config/importmap.rb
+++ config/importmap.rb
@@ -3,6 +3,6 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "jquery", to: "https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.min.js"
-pin "jquery-ui", to: "https://code.jquery.com/ui/1.13.3/jquery-ui.min.js"
+pin "jquery", to: "https://cdn.jsdelivr.net/npm/jquery@2.2.1/dist/jquery.js"
+pin "jquery-ui", to: "https://code.jquery.com/ui/1.12.1/jquery-ui.js"
 pin 'autocomplete', preload: true
